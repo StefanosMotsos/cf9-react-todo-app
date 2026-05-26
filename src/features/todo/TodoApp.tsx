b4ce7a9dd3852d04189a5fcc4427838afe3b9394
Στέφανος Μότσος <stefanosmotsos01@gmail.com>
2026-05-26 23:10:05 +0300

    feat(todo): add TodoStats component

--- src/features/todo/TodoApp.tsx
+++ src/features/todo/TodoApp.tsx
@@ -2,15 +2,17 @@ import TodoForm from "./TodoForm.tsx";
 import {useRef} from "react";
 import {useTodos} from "./hooks/useTodos.ts";
 import TodoList from "./TodoList.tsx";
+import TodoStats from "./TodoStats.tsx";
+import Button from "../../shared/ui/Button.tsx";
 
 const TodoApp = () => {
     const inputRef = useRef<HTMLInputElement>(null);
+    const { todos, addTodo, deleteTodo,editTodo, toggleTodo, clearAll } = useTodos();
 
-    const { todos, addTodo, editTodo, deleteTodo, toggleTodo, clearAll } = useTodos();
-
-    const handleAdd = (text:string) => {
-        addTodo(text)
-        inputRef.current?.focus()
+    const handleAdd = (text: string) => {
+        addTodo(text);
+        inputRef.current?.focus();
+        console.log(todos);
     }
 
     return (
@@ -19,12 +21,25 @@ const TodoApp = () => {
                 <h1 className="text-center text-2xl py-8">To-Do List</h1>
 
                 <TodoForm onAdd={handleAdd} inputRef={inputRef} />
+
                 <TodoList
                     todos={todos}
                     onToggle={toggleTodo}
                     onEdit={editTodo}
                     onDelete={deleteTodo}
                 />
+
+                <TodoStats todos={todos}/>
+
+                {todos.length > 0 && (
+                    <div className="text-end mt-4">
+                        <Button
+                            addClasses="bg-cf-dark-red"
+                            label="Clear All"
+                            onClick={clearAll}
+                        />
+                    </div>
+                )}
 
             </div>
         </>
